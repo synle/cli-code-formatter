32048dffcfbf17cb9888428c7f784c9997fdee2c
Sy Le <sle@salesforce.com>
2016-07-11 08:38:51 -0700

    support diffing 2 different git sha

--- code-differ.js
+++ code-differ.js
@@ -12,39 +12,64 @@ var formatter = require('./formatter');
 var timeStamp = Date.now();
 var fileNamePrefix = '/tmp/code-differ-' + timeStamp
 
-var differTool = process.argv[4] || 'opendiff $file1 $file2'; //fall back to vimdiff
+var argLenth = process.argv.length;
 
 //if inputs are file
-var fileName1 = process.argv[2];
-var fileName2 = process.argv[3];
+var inFileName1 = process.argv[2];
+var inFileName2 = process.argv[3];
 
 
 //if input are commit hash
 var commitHash = process.argv[2];
 var fileName = process.argv[3];
+var sha1 = commitHash;
+var sha2 = commitHash + '^'
 
 var isFileMode = commitHash.length !== 7 && commitHash.length !== 40; //sha hash is either 7 or 40 (long or short)
+if (argLenth >= 5) {
+    //when compare 2 hashes
+    sha2 = process.argv[3];
+    fileName = process.argv[4];
+    isFileMode = false;
+}
+
+//get difftool
+var differTool;
+for (var i = 0; i < argLenth; i++) {
+    if (process.argv[i].indexOf('$file1') >= 0 && process.argv[i].indexOf('$file2') >= 0) {
+        differTool = process.argv[i]; //set it
+    }
+}
+differTool = differTool || 'opendiff $file1 $file2'; //fall back to vimdiff
+
 
 //this will work with file path
 new Promise(function(fulfill, reject) {
     //first try reading input as files
     if (isFileMode === true) {
-        formatter(fileName1).then(function(formattedContent1) {
-            formatter(fileName2).then(function(formattedContent2) {
+        console.log('Diffing Files');
+        console.log('====Input');
+        console.log('inFileName1', inFileName1);
+        console.log('inFileName2', inFileName2);
+
+        formatter(inFileName1).then(function(formattedContent1) {
+            formatter(inFileName2).then(function(formattedContent2) {
                 fulfill([formattedContent1, formattedContent2]);
             })
         });
     } else {
+        console.log('Diffing SHA');
+        console.log('====Input');
+
+        console.log('sha1', sha1);
+        console.log('sha2', sha2);
+
         //format the path for filename
         fileName = fileName;
 
-        //sha to compare
-        var sha1 = commitHash;
-        var sha2 = commitHash + '^'
-
         //need to retry as commit hash
         var gitCatFileCmd1 = 'git cat-file blob ' + sha1 + ':./' + fileName;
-        var gitCatFileCmd2 = 'git cat-file blob ' + sha2 + '^:./' + fileName;
+        var gitCatFileCmd2 = 'git cat-file blob ' + sha2 + ':./' + fileName;
 
         exec(gitCatFileCmd1, function(err1, formattedContent1) {
             exec(gitCatFileCmd2, function(err2, formattedContent2) {
@@ -71,10 +96,10 @@ new Promise(function(fulfill, reject) {
         var diffToolCmd = differTool.replace('$file1', '"' + outFileName1 + '"')
             .replace('$file2', '"' + outFileName2 + '"');
 
-        console.log('====');
+        console.log('====output');
         console.log('outFileName1: ', outFileName1);
         console.log('outFileName2: ', outFileName2);
-        console.log('====');
+        console.log('====diff command');
         console.log(diffToolCmd);
 
         //execute the differ command
